ab273b23df2a95803258573b193880f4bac0e0b2
SANDIP PAUL <sandip@htzoop.com>
2024-02-21 14:21:01 +0530

    fs

---
+++ src/components/Nav/Quote.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+import Button from "../Global_components/Button";
+class Quote extends Component {
+  render() {
+    return <Button content="Get a Quote" />;
+  }
+}
+export default Quote;
